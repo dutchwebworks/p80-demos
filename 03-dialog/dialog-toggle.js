db6a25d2d8e5dac5b0b837d3cbebfcee90d462ff
Dennis Burger <dennis.burger@poort80.nl>
2023-06-15 09:31:03 +0200

    Working return value demo

--- 03-dialog/dialog-toggle.js
+++ 03-dialog/dialog-toggle.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const dialog2ReturnValue = document.getElementById("dialog2ReturnValue");
+
+
+  // Open dialogs
+  // --------------------------------------------------------------
+
   document.querySelectorAll("[data-dialog-open]").forEach((button) => {
     button.addEventListener("click", (event) => {
+      dialog2ReturnValue.innerText = "";
+
       if(event.target.hasAttribute("data-tooltip")) {
         document.getElementById(button.dataset.dialogOpen).show();
       } else {
@@ -9,11 +17,19 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
+
+  // Close dialogs
+  // --------------------------------------------------------------
+
   document.querySelectorAll("[data-dialog-close]").forEach((button) => {
     button.addEventListener("click", (event) => {
-      event.target.closest("dialog").close();
+      event.target.closest("dialog").close(event.target.value);
     });
   });
+
+
+  // Close dialogs when clicking on the dimmed background
+  // --------------------------------------------------------------
 
   document.querySelectorAll("dialog").forEach((dialog) => {
     dialog.addEventListener("click", (event) => {
@@ -28,5 +44,21 @@ document.addEventListener("DOMContentLoaded", () => {
         dialog.close();
       }
     });
+  });
+
+
+  // Dialog 2, return value
+  // --------------------------------------------------------------  
+
+  const dialog2 = document.getElementById("dialog2");
+  const dialog2ConfirmButton = document.getElementById("dialog2ConfirmButton");  
+
+  dialog2.addEventListener("close", event => {
+    dialog2ReturnValue.innerText = `Return value: "${dialog2.returnValue}"`;
+  });
+
+  dialog2ConfirmButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    dialog2.close(dialog2ConfirmButton.value);
   });
 });
