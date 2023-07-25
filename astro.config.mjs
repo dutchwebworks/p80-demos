c0b8ac5ed9c524013c7781988e5a74377548de94
Dennis Burger <dennis@dutchwebworks.nl>
2023-07-25 10:26:37 +0200

    Integrated the first page into Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  trailingSlash: 'never',
+  site: "https://p80-demos.dutchwebworks.nl",
   integrations: [tailwind()]
 });
